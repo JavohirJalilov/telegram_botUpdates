161acbb41e1666882208389a5aa322d73ad3dc87
Javohir <javohirjalilov777@gmail.com>
2020-09-29 18:09:49 +0500

    getUpdates and sendMessage Function

--- Main.py
+++ Main.py
@@ -2,13 +2,29 @@ import requests
 import json
 from pprint import pprint
 
-r = requests.get('https://api.telegram.org/bot1391170757:AAFlwFdRBaE-Io72LpLKXMN1KmjXlcLfedo/getUpdates')
+def sendMessage(chat_id, text):
+    url = 'https://api.telegram.org/bot1391170757:AAFlwFdRBaE-Io72LpLKXMN1KmjXlcLfedo/sendMessage'
+    parameter = {
+        'chat_id': chat_id,
+        'text': text
+    }
+    response = requests.get(url=url, params=parameter)
 
-data = r.json()
 
-message = data['result'][0]['message']
-from1 = message["from"]
-chat = message["chat"]
-date = message['date']
-txt = message["text"]
-pprint(from1)
+def getUpdates():
+    chat_id = []
+    r = requests.get('https://api.telegram.org/bot1391170757:AAFlwFdRBaE-Io72LpLKXMN1KmjXlcLfedo/getUpdates')
+    data = r.json()
+    message = data['result']
+    for i in message:
+        chat_idx = i['message']["chat"]['id']
+        chat_id.append(chat_idx)
+    return chat_id
+    
+
+
+chat_id = getUpdates()
+for idx in chat_id:
+    sendMessage(chat_id, f'Hello!')
+    
+    
