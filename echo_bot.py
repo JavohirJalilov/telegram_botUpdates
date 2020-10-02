0a3d47b4d9984fddc111ca8df378b36c6dbac27c
Javohir <javohirjalilov777@gmail.com>
2020-10-02 10:20:06 +0500

    echo bot one

--- echo_bot.py
+++ echo_bot.py
@@ -1,26 +1,46 @@
 import requests
 import json
 
-def sendMessage(chat_id,text):
-    url = 'https://api.telegram.org/bot1391170757:AAFlwFdRBaE-Io72LpLKXMN1KmjXlcLfedo/sendMessage'
-    parametr = {"chat_id":chat_id,"text":text}
-    respons = requests.get(url = url, params= parametr)
+Token = '1302971549:AAGx9wgPHvKZQtLpV29iMkmqGZTPqR6UQeE'
 
+def getUpdates():
+    url = f'https://api.telegram.org/bot{Token}/getUpdates'
+    response = requests.get(url = url)
+    update = response.json()
 
-def getUpdates():   
-    r = requests.get('https://api.telegram.org/bot1391170757:AAFlwFdRBaE-Io72LpLKXMN1KmjXlcLfedo/getUpdates')
-    data = r.json()
+    return update
+
+def get_message(update):
+    txt = update['result'][-1]['message']['text']
+    update_id = update['result'][-1]['update_id']
+
+    return txt, update_id
+
+def sendMessage(chat_id,txt):
+    url = f'https://api.telegram.org/bot{Token}/sendMessage'
+    params = {
+        'chat_id':chat_id,
+        'text':txt,
+        'disable_notification': True
+    }
+    response = requests.get(url = url,params = params)
+
+def updates():
+    url = f'https://api.telegram.org/bot{Token}/getUpdates'
+    response = requests.get(url = url)
+    data = response.json()
     chat_id = data['result'][-1]['message']['chat']['id']
     return chat_id
+    
 
 
-def getUpdatestxt():
-    r = requests.get('https://api.telegram.org/bot1391170757:AAFlwFdRBaE-Io72LpLKXMN1KmjXlcLfedo/getUpdates')
-    data = r.json()
-    text = data['result'][-1]['message']['text']
-    return text
 
-chat_id = getUpdates()
-text = getUpdatestxt()
-
-sendMessage(chat_id,text)
+last_update_id = -1
+while True:
+    x = getUpdates()
+    txt, update_id = get_message(x)
+    chat_id = updates()
+    print(f'last_update_id: {last_update_id} = update_id: {update_id}')
+    if last_update_id != update_id:
+        sendMessage(chat_id,txt)
+        last_update_id = update_id
